a66050d85f4ca782b7828a2d017c82687af6d933
Daniela Salas <danielapiconsalas@gmail.com>
2023-07-10 13:28:22 -0500

    finished the remove last problem

--- src/main/java/datastructures/LinkedList/LinkedListPractice.java
+++ src/main/java/datastructures/LinkedList/LinkedListPractice.java
@@ -108,4 +108,43 @@ public class LinkedListPractice {
     }
 
 
+//    Remove Last
+//    Implement a method called removeLast that removes the last node from the linked list.
+    public Node removeLast(){
+//      If the length of the linked list is 0, return null.
+        if(length == 0){
+            return null;
+        }
+
+//      Create two variables that can point to a Node , temp and pre, both initially pointing to the head of the linked list.
+        Node temp = head;
+        Node pre = head;
+
+
+//      Traverse the linked list until temp.next is null, performing the following tasks during traversal:
+//      Update pre to point to the current temp node.
+//      Update temp to point to the next node in the linked list.
+        while(temp.next != null){
+            pre = temp;
+            temp = temp.next;
+        }
+
+//      After traversal, set the tail pointer to pre.
+//      Set the next attribute of the tail node to null.
+//      Decrement the length attribute of the list by 1.
+        tail = pre;
+        tail.next = null;
+        length--;
+
+//      If the length of the linked list becomes 0 after removing the last node, set both the head and tail pointers to null.
+        if(length == 0){
+            head = null;
+            tail= null;
+        }
+
+//      Return the removed node (pointed to by temp).
+        return temp;
+    }
+
+
 }
