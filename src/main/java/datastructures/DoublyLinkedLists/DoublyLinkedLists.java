2a697147d3d3413b8ecfd740160b63a592bcc276
Daniela Salas <danielapiconsalas@gmail.com>
2023-07-11 11:47:28 -0500

    finished the append method for Doubly Linked Lists

--- src/main/java/datastructures/DoublyLinkedLists/DoublyLinkedLists.java
+++ src/main/java/datastructures/DoublyLinkedLists/DoublyLinkedLists.java
@@ -42,4 +42,35 @@ public class DoublyLinkedLists {
     public void getLength() {
         System.out.println("Length: " + length);
     }
+
+
+//    Append
+//    Implement the append method that appends a new node to the end of the doubly linked list.
+//
+//    Return type: void
+
+//    Accept an integer value as an argument, which will be the value of the new node.
+    public void append(int value){
+//      Create a new Node object called newNode with the given value.
+        Node newNode = new Node(value);
+
+//        If the length of the doubly linked list is 0:
+//        Set both the head and tail pointers of the list to the newNode.
+        if(length == 0){
+            head = newNode;
+            tail = newNode;
+        }else{
+//          If the length of the doubly linked list is greater than 0:
+//          Set the next attribute of the current tail node to the newNode.
+//          Set the prev attribute of the newNode to the current tail node.
+//          Update the tail pointer of the list to point to the newNode.
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+
+//      Increment the length attribute of the list by 1.
+        length++;
+    }
+
 }
