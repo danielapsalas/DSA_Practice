393f9bed27a6df254b8da0e5e3f2174c42e51b83
Daniela Salas <danielapiconsalas@gmail.com>
2023-07-13 17:27:54 -0500

    Completed the Reverse method for DLL

--- src/main/java/datastructures/DoublyLinkedLists/DoublyLinkedLists.java
+++ src/main/java/datastructures/DoublyLinkedLists/DoublyLinkedLists.java
@@ -360,4 +360,42 @@ public class DoublyLinkedLists {
         }
     }
 
+
+
+//    DLL: Reverse ( ** Interview Question)
+//    Implement a method called reverse() that reverses the order of the nodes in the list.
+//    Method Signature:
+//    public void reverse()
+//    Output:
+//    No explicit output is returned. However, the method should modify the doubly linked list such that the order of the nodes is reversed.
+//    Constraints:
+//    The doubly linked list may be empty or have one or more nodes.
+//    Example:
+//    Consider the following doubly linked list:
+//    Head: 1
+//    Tail: 5
+//    Length: 5
+//    Doubly Linked List:
+//    1 <-> 2 <-> 3 <-> 4 <-> 5
+//    After calling reverse(), the list should be:
+//    Head: 5
+//    Tail: 1
+//    Length: 5
+//    Doubly Linked List:
+//    5 <-> 4 <-> 3 <-> 2 <-> 1
+    public void reverse(){
+        Node current = head;
+        Node temp = null;
+
+        while (current != null) {
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev;
+        }
+
+        temp = head;
+        head = tail;
+        tail = temp;
+    }
 }
