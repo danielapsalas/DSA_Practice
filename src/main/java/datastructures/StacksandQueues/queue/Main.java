cc054b7e198030ea556a3d09f84dca3f14148ae3
Daniela Salas <danielapiconsalas@gmail.com>
2023-07-16 11:36:01 -0500

    completed the constructor method for queues

---
+++ src/main/java/datastructures/StacksandQueues/queue/Main.java
@@ -0,0 +1,16 @@
+package datastructures.StacksandQueues.queue;
+
+public class Main {
+    public static void main(String[] args) {
+//        //constructor
+//        Queue myQueue = new Queue(7);
+//        myQueue.getFirst();
+//        myQueue.getLast();
+//        myQueue.getLength();
+
+        //Enqueue
+        Queue myQueue = new Queue(1);
+        myQueue.printQueue();
+
+    }
+}
---
+++ src/main/java/datastructures/StacksandQueues/queue/Queue.java
@@ -0,0 +1,46 @@
+package datastructures.StacksandQueues.queue;
+public class Queue {
+    private Node first;
+    private Node last;
+    private int length;
+
+    class Node{
+        int value;
+        Node next;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+
+    public Queue(int value){
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    public void printQueue(){
+        Node temp = first;
+        while(temp != null){
+            System.out.println(temp);
+            temp = temp.next;
+        }
+    }
+
+    public void getFirst(){
+        System.out.println("First: " + first.value);
+    }
+
+    public void getLast(){
+        System.out.println("Last: " + last.value);
+    }
+
+    public void getLength(){
+        System.out.println("Length: " + length);
+    }
+
+
+
+
+}
