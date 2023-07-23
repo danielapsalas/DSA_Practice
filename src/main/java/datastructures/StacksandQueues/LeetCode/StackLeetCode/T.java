3841b3326ec2bb19966e10d7f9bbd504ba09362b
Daniela Salas <danielapiconsalas@gmail.com>
2023-07-23 13:11:12 -0500

    Completed a method named Push for a Stack That Uses an ArrayList ( ** Interview Question)

--- src/main/java/datastructures/StacksandQueues/LeetCode/StackLeetCode/T.java
+++ src/main/java/datastructures/StacksandQueues/LeetCode/StackLeetCode/T.java
@@ -31,6 +31,20 @@ public class T {
         return stackList.size();
     }
 
+
+//    Stack: Push for a Stack That Uses an ArrayList ( ** Interview Question)
+//    In the Stack: Intro video we discussed how stacks are commonly implemented using an ArrayList instead of a Linked List.
+//
+//    Write the push method to add an item onto a stack that is implemented with an ArrayList.
+//
+//    We will modify the Stack class to be able to store multiple data types by using a generic parameter type <T>. This will allow the class to hold Integers, Characters or any other data type.
+//
+//    Return type: void
+    public void push(T value){
+        stackList.add(value);
+    }
+
+
 //    Stack: Pop for a Stack That Uses an ArrayList ( ** Interview Question)
 //    Add a method to pop a value from the Stack implementation that we began in the last Coding Exercise.
 //
